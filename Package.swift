dd85a34fc14ab1bdd25565d3d1f13e26bbb9d026
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:25:42 +0100

    Update dependencies to latest versions

--- Package.swift
+++ Package.swift
@@ -39,11 +39,11 @@ let package = Package(
         .library(name: .rfc5646, targets: [.rfc5646])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.8.0"),
-        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.6.0"),
-        .package(url: "https://github.com/swift-standards/swift-iso-639", from: "0.2.0"),
-        .package(url: "https://github.com/swift-standards/swift-iso-3166", from: "0.2.0"),
-        .package(url: "https://github.com/swift-standards/swift-iso-15924", from: "0.1.0")
+        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.10.0"),
+        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.6.2"),
+        .package(url: "https://github.com/swift-standards/swift-iso-639", from: "0.2.1"),
+        .package(url: "https://github.com/swift-standards/swift-iso-3166", from: "0.2.1"),
+        .package(url: "https://github.com/swift-standards/swift-iso-15924", from: "0.1.1")
     ],
     targets: [
         .target(
